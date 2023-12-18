6d89c2153851b2585cc55b72a4853ae5b0024d15
serdnaman <122748106+serdnaman@users.noreply.github.com>
2023-12-18 01:49:50 +0100

    proyecto

--- app/src/main/java/com/example/proyectopelisappcine/roomdb/FavoriteDAO.java
+++ app/src/main/java/com/example/proyectopelisappcine/roomdb/FavoriteDAO.java
@@ -1,10 +1,8 @@
 package com.example.proyectopelisappcine.roomdb;
 
-import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
-import androidx.room.Query;
 
 import com.example.proyectopelisappcine.model.Favorite;
 
